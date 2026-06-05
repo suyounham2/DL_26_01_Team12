b72b722ae1a34414a666db25909ac74d5b5973a5
binnn46-collab <bansubin46@gmail.com>
2026-06-06 00:15:53 +0900

    Update send_alert.py

--- core/send_alert.py
+++ core/send_alert.py
@@ -5,7 +5,7 @@ from config import DISCORD_WEBHOOK_URL
 def send_discord_alert():
 
     data = {
-        "content": "🚨 FALL DETECTED! 낙상이 감지되었습니다."
+        "content": "🚨 보호 대상자에게서 낙상 의심 상황이 감지되었습니다. 즉시 상태를 확인해주세요."
     }
 
     response = requests.post(
